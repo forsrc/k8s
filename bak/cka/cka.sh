1992548acf4147259595646767611c64fb2a07cf
forsrc <forsrc@gmail.com>
2019-11-28 21:34:12 +0800

    Update cka.sh

--- bak/cka/cka.sh
+++ bak/cka/cka.sh
@@ -8,34 +8,46 @@ source <(kubectl completion bash)
 
 ################ static pod
 
+cat /var/lib/kubelet/config.yaml  # staticPodPath: /etc/kubernetes/manifests
+
 mkdir /etc/kubelet.d
-cat <<EOF >/etc/kubelet.d/static-web.yaml
+cat <<EOF > /etc/kubernetes/manifests/static-web.yaml
 apiVersion: v1
 kind: Pod
 metadata:
-name: static-web
-labels:
-role: myrole
+  name: static-web
+  labels:
+    role: myrole
 spec:
-containers:
-- name: web
-  image: nginx
-  ports:
+  containers:
     - name: web
-      containerPort: 80
-      protocol: TCP
+      image: nginx
+      ports:
+        - name: web
+          containerPort: 80
+          protocol: TCP
 EOF
 
 find / -name kubelet.service.d
 
 # vi /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
-# --pod-manifest-path=/etc/kubelet.d/
+# --pod-manifest-path=/etc/kubernetes/manifests"
+# or
+# --manifest-ur=...
 
 find / -name kubelet
-echo 'KUBELET_EXTRA_ARGS="--cluster-dns=10.96.0.10 --cluster-domain=cluster.local --pod-manifest-path=/etc/kubelet.d/"' >/etc/sysconfig/kubelet
+# echo 'KUBELET_EXTRA_ARGS="--cluster-dns=10.96.0.10 --cluster-domain=cluster.local --pod-manifest-path=/etc/kubernetes/manifests"' >/etc/sysconfig/kubelet
+echo 'KUBELET_EXTRA_ARGS="--pod-manifest-path=/etc/kubernetes/manifests"' >/etc/sysconfig/kubelet
 
 systemctl daemon-reload
 systemctl restart kubelet
-systemctl status kubelet
+systemctl status kubelet -l
 
-################
+ps -ef | grep kubelet
+
+##################
+
+
+
+
+#################
