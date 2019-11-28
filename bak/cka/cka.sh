99862cef3ba99f1b380ddf3090de53e222699893
forsrc <forsrc@gmail.com>
2019-11-28 19:33:06 +0800

    Update cka.sh

--- bak/cka/cka.sh
+++ bak/cka/cka.sh
@@ -6,5 +6,36 @@ source /usr/share/bash-completion/bash_completion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 source <(kubectl completion bash)
 
-################
+################ static pod
 
+
+cat <<EOF >/etc/kubernetes/manifests/static-web.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+name: static-web
+labels:
+role: myrole
+spec:
+containers:
+- name: web
+  image: nginx
+  ports:
+    - name: web
+      containerPort: 80
+      protocol: TCP
+EOF
+
+find / -name kubelet.service.d
+
+# vi /usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf
+# --pod-manifest-path=/etc/kubelet.d/
+
+find / -name kubelet
+echo 'KUBELET_EXTRA_ARGS="--cluster-dns=10.96.0.10 --cluster-domain=cluster.local --pod-manifest-path=/etc/kubelet.d/"' >/etc/sysconfig/kubelet
+
+systemctl daemon-reload
+systemctl restart kubelet
+systmectl status kubelet
+
+################
