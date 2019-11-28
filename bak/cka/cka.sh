4631276c8a064dcccc270872aede42d57b0c06d8
forsrc <forsrc@gmail.com>
2019-11-28 18:19:18 +0800

    Update cka.sh

--- bak/cka/cka.sh
+++ bak/cka/cka.sh
@@ -1,1 +1,10 @@
 
+################ kubectl completion bash
+
+yum install bash-completion -y
+source /usr/share/bash-completion/bash_completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+source <(kubectl completion bash)
+
+################
+
