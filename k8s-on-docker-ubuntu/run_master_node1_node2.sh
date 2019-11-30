7c8764380fe6a1398c90744f6f958700c72c0e12
forsrc <forsrc@gmail.com>
2019-11-30 14:03:01 +0800

    Create run_master_node1_node2.sh

---
+++ k8s-on-docker-ubuntu/run_master_node1_node2.sh
@@ -0,0 +1,12 @@
+NODE_NAME=master
+NODE_IP=172.10.0.10
+docker run -d -it --privileged=true  -d -it --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/initdocker run -d -it --privileged=true  -d -it --ip 172.10.0.10  -v /temp:/temp/ -v /k8s-on-docker/var/lib/:/var/lib/ -v /k8s-on-docker/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/etc/docker/:/etc/docker/ -v /k8s-on-docker/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name k8s-on-docker forsrc/ubuntu:k8s /sbin/init
+
+NODE_NAME=node1
+NODE_IP=172.10.0.11
+docker run -d -it --privileged=true  -d -it --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/initdocker run -d -it --privileged=true  -d -it --ip 172.10.0.10  -v /temp:/temp/ -v /k8s-on-docker/var/lib/:/var/lib/ -v /k8s-on-docker/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/etc/docker/:/etc/docker/ -v /k8s-on-docker/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name k8s-on-docker forsrc/ubuntu:k8s /sbin/init
+
+
+NODE_NAME=node2
+NODE_IP=172.10.0.12
+docker run -d -it --privileged=true  -d -it --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/initdocker run -d -it --privileged=true  -d -it --ip 172.10.0.10  -v /temp:/temp/ -v /k8s-on-docker/var/lib/:/var/lib/ -v /k8s-on-docker/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/etc/docker/:/etc/docker/ -v /k8s-on-docker/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name k8s-on-docker forsrc/ubuntu:k8s /sbin/init
