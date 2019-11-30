cd404659851ecb12b0162db6e73f3e99c03ce6b2
forsrc <forsrc@gmail.com>
2019-11-30 16:21:47 +0800

    Update run_master_node1_node2.sh

--- k8s-on-docker-ubuntu/run_master_node1_node2.sh
+++ k8s-on-docker-ubuntu/run_master_node1_node2.sh
@@ -12,6 +12,7 @@ NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/"
+NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/etc/resolv.conf:/etc/resolv.conf"
 docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
 NODE_NAME=node1
@@ -21,6 +22,7 @@ NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/"
+NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/etc/resolv.conf:/etc/resolv.conf"
 docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
 NODE_NAME=node2
@@ -30,4 +32,5 @@ NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/"
 NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/"
+NODE_VOLUME="$NODE_VOLUME -v /k8s-on-docker/etc/resolv.conf:/etc/resolv.conf"
 docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
