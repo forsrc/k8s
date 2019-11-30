1ff2e8777b6cfe483fdeb61b9ae29d26ee7c15d3
forsrc <forsrc@gmail.com>
2019-11-30 14:15:10 +0800

    Update run_master_node1_node2.sh

--- k8s-on-docker-ubuntu/run_master_node1_node2.sh
+++ k8s-on-docker-ubuntu/run_master_node1_node2.sh
@@ -2,6 +2,7 @@ NODE_NAME=master
 NODE_IP=172.10.0.10
 docker run -d -it --privileged=true  -d -it --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
+NODE_NAME=node1
 NODE_IP=172.10.0.11
 docker run -d -it --privileged=true  -d -it --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
