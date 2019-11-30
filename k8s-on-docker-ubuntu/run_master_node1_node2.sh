7f7baa1e5f5013429c31e4fb9e1d1bc8f8b5c0ff
forsrc <forsrc@gmail.com>
2019-11-30 14:47:41 +0800

    Update run_master_node1_node2.sh

--- k8s-on-docker-ubuntu/run_master_node1_node2.sh
+++ k8s-on-docker-ubuntu/run_master_node1_node2.sh
@@ -2,12 +2,12 @@ docker network create --subnet=172.7.0.0/24 --gateway=172.7.0.1 net-ubuntu-k8s
 
 NODE_NAME=master
 NODE_IP=172.7.0.10
-docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
+docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
 NODE_NAME=node1
 NODE_IP=172.7.0.11
-docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
+docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
 
 NODE_NAME=node2
 NODE_IP=172.7.0.12
-docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname centos-k8s-master --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
+docker run -d -it --privileged=true  -d -it --network net-ubuntu-k8s --ip $NODE_IP  -v /temp:/temp/ -v /k8s-on-docker/$NODE_NAME/var/lib/:/var/lib/ -v /k8s-on-docker/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/ -v /k8s-on-docker/$NODE_NAME/etc/docker/:/etc/docker/ -v /k8s-on-docker/$NODE_NAME/k8s/:/k8s-on-docker/ --hostname $NODE_NAME --name $NODE_NAME forsrc/ubuntu:k8s /sbin/init
