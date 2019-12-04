687d867435eb98379248b644755708c2b407c9ae
forsrc <forsrc@gmail.com>
2019-12-04 10:22:29 +0800

    Update run_master_node1_node2.sh

--- k8s-on-docker-ubuntu/run_master_node1_node2.sh
+++ k8s-on-docker-ubuntu/run_master_node1_node2.sh
@@ -10,7 +10,7 @@ NODE_IMAGE=forsrc/ubuntu:k8s
 NODE_NAME=master
 NODE_IP=172.7.0.10
 NODE_VOLUME=
-NODE_VOLUME="$NODE_VOLUME -v /temp:/temp/"
+NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/temp:/temp/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/docker/:/var/lib/docker/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/kubelet/:/var/lib/kubelet/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
@@ -23,7 +23,7 @@ NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/etc/resolv.conf:/etc/resolv.conf"
 NODE_PORT="-p 30000:30000 -p 30080:30080 -p 30443:30443 -p 8080:8080 -p 30001:30001 -p 30002:30002 -p 30003:30003 -p 30004:30004 -p 30005:30005"
 
 
-docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME $NODE_PORT --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE /sbin/init
+docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME $NODE_PORT --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE
 
 #apt-get install bash-completion -y
 echo 'source /usr/share/bash-completion/bash_completion' >>/k8s-on-docker/$NODE_NAME/root/.bashrc
@@ -33,7 +33,7 @@ echo 'source <(kubectl completion bash)' >>/k8s-on-docker/$NODE_NAME/root/.bashr
 NODE_NAME=node1
 NODE_IP=172.7.0.11
 NODE_VOLUME=
-NODE_VOLUME="$NODE_VOLUME -v /temp:/temp/"
+NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/temp:/temp/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/docker/:/var/lib/docker/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/kubelet/:/var/lib/kubelet/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
@@ -43,12 +43,12 @@ NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/etc/cni/:/etc/cni/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/k8s/:/k8s-on-docker/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/root/:/root/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/etc/resolv.conf:/etc/resolv.conf"
-docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE /sbin/init
+docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE
 
 NODE_NAME=node2
 NODE_IP=172.7.0.12
 NODE_VOLUME=
-NODE_VOLUME="$NODE_VOLUME -v /temp:/temp/"
+NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR//temp:/temp/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/docker/:/var/lib/docker/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/var/lib/kubelet/:/var/lib/kubelet/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/etc/kubernetes/:/etc/kubernetes/"
@@ -58,4 +58,4 @@ NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/etc/cni/:/etc/cni/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/k8s/:/k8s-on-docker/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/$NODE_NAME/root/:/root/"
 NODE_VOLUME="$NODE_VOLUME -v $NODE_DATA_DIR/etc/resolv.conf:/etc/resolv.conf"
-docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE /sbin/init
+docker run -d -it --privileged=true -d -it --network $NODE_NETWORK --ip $NODE_IP $NODE_VOLUME --hostname $NODE_NAME --name $NODE_NAME $NODE_IMAGE
