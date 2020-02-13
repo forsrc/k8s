f8ff47ca3f17ecdb35e3e5283b8f01b7e28c70b3
forsrc <forsrc@gmail.com>
2020-02-14 00:28:05 +0900

    k8s-nodejs

---
+++ k8s-nodejs/app/index.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+ console.log("k8s-nodejs")
+
+const k8s = require('@kubernetes/client-node');
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+
+k8sApi.listNamespacedPod('default').then((res) => {
+    console.log(res.body);
+});
